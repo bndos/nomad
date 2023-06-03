0ca5e051e1b71c2cf16655e28070cca0d6e8204c
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-03 01:55:50 -0400

    feat: Adds draggable bottom container (#9)

--- lib/widgets/map/place_details_bottom_sheet.dart
+++ lib/widgets/map/place_details_bottom_sheet.dart
@@ -1,54 +1,80 @@
 import 'package:flutter/material.dart';
 
-class PlaceDetailsBottomSheet extends StatelessWidget {
+class PlaceDetailsContainer extends StatefulWidget {
   final String placeName;
   final String address;
 
-  const PlaceDetailsBottomSheet({
+  const PlaceDetailsContainer({
     Key? key,
     required this.placeName,
     required this.address,
   }) : super(key: key);
 
   @override
+  PlaceDetailsContainerState createState() => PlaceDetailsContainerState();
+}
+
+class PlaceDetailsContainerState extends State<PlaceDetailsContainer> {
+  double containerHeight = 200.0; // Initial height of the container
+  double dragOffset = 0.0; // Offset for tracking the drag gesture
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            placeName,
-            style: const TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 8.0),
-          Text(
-            address,
-            style: const TextStyle(fontSize: 16.0),
-          ),
-          const SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: GestureDetector(
+        onVerticalDragUpdate: (details) {
+          setState(() {
+            // Update the container height based on the vertical drag gesture
+            containerHeight -= details.delta.dy;
+            // Ensure the container doesn't exceed certain limits
+            containerHeight = containerHeight.clamp(
+                100.0, MediaQuery.of(context).size.height);
+          });
+        },
+        child: Container(
+          width: double.infinity,
+          height: containerHeight,
+          color: Colors.white,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  // Perform action 1
-                },
-                child: const Text('Action 1'),
+              Text(
+                widget.placeName,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // Perform action 2
-                },
-                child: const Text('Action 2'),
+              const SizedBox(height: 8.0),
+              Text(
+                widget.address,
+                style: const TextStyle(fontSize: 16.0),
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Perform action 1
+                    },
+                    child: const Text('Action 1'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Perform action 2
+                    },
+                    child: const Text('Action 2'),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
