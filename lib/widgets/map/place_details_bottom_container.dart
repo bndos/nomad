c9b77a07372fb136934a8918882708c2877aa7c5
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-03 16:08:06 -0400

    feat: Removes the overflow error (#9)

--- lib/widgets/map/place_details_bottom_container.dart
+++ lib/widgets/map/place_details_bottom_container.dart
@@ -31,48 +31,68 @@ class PlaceDetailsContainerState extends State<PlaceDetailsContainer> {
             containerHeight -= details.delta.dy;
             // Ensure the container doesn't exceed certain limits
             containerHeight = containerHeight.clamp(
-                100.0, MediaQuery.of(context).size.height);
+              100.0,
+              MediaQuery.of(context).size.height,
+            );
           });
         },
         child: Container(
           width: double.infinity,
           height: containerHeight,
-          color: Colors.white,
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.placeName,
-                style: const TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                widget.address,
-                style: const TextStyle(fontSize: 16.0),
-              ),
-              const SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Perform action 1
-                    },
-                    child: const Text('Action 1'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Perform action 2
-                    },
-                    child: const Text('Action 2'),
-                  ),
-                ],
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 20.0,
+                spreadRadius: 5.0,
               ),
             ],
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              topRight: Radius.circular(16.0),
+            ),
+          ),
+          child: OverflowBox(
+            minHeight: 0.0,
+            maxHeight: double.infinity,
+            alignment: Alignment.topCenter,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.placeName,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  widget.address,
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Perform action 1
+                      },
+                      child: const Text('Action 1'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Perform action 2
+                      },
+                      child: const Text('Action 2'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
