9a1fc9bb0bee1c256d91366dfbd5feff9bb0c1c6
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-03 21:36:20 -0400

    feat: Adds distances to places (#9)

---
+++ lib/widgets/map/rounded_icon_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class RoundedIconButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback? onPressed;
+
+  const RoundedIconButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 4.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            elevation: 0, // Set elevation to 0 to remove the shadow
+            backgroundColor: Colors.grey[100], // Pale grey background color
+            foregroundColor: Colors.black, // Black foreground color
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          ),
+          onPressed: onPressed,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(icon, size: 16.0),
+              const SizedBox(width: 8.0),
+              Text(
+                label,
+                style: const TextStyle(fontSize: 12.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
