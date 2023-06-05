930ace912860b0ab88bbadaea45fdeb41e3499f3
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-05 18:53:58 -0400

    feat: Fixes exception of parent (#9)

--- lib/widgets/map/search_field.dart
+++ lib/widgets/map/search_field.dart
@@ -19,53 +19,49 @@ class SearchField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: 30,
-      left: 20,
-      right: 20,
-      child: Container(
-        width: Get.width,
-        padding: const EdgeInsets.only(left: 5),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 6,
-              offset: Offset(0, 2),
+    return Container(
+      width: Get.width,
+      margin: const EdgeInsets.only(top: 6),
+      padding: const EdgeInsets.only(left: 5),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 2),
+          ),
+        ],
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Row(
+        children: [
+          if (predictions.isNotEmpty)
+            IconButton(
+              icon: const Icon(Icons.clear),
+              onPressed: clearPredictions,
+            )
+          else
+            const IconButton(
+              icon: Icon(Icons.search),
+              onPressed: null,
             ),
-          ],
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Row(
-          children: [
-            if (predictions.isNotEmpty)
-              IconButton(
-                icon: const Icon(Icons.clear),
-                onPressed: clearPredictions,
-              )
-            else
-              const IconButton(
-                icon: Icon(Icons.search),
-                onPressed: null,
+          Expanded(
+            child: TextFormField(
+              controller: searchController,
+              onChanged: (value) => onSearchChanged(),
+              style: GoogleFonts.poppins(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
               ),
-            Expanded(
-              child: TextFormField(
-                controller: searchController,
-                onChanged: (value) => onSearchChanged(),
-                style: GoogleFonts.poppins(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Search',
-                ),
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search',
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
