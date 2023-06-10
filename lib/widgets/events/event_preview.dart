9ceaf4c6f8888aab68d848f9d2495a10d706e16f
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-10 15:00:28 -0400

    feat: Improves event preview (#11)

--- lib/widgets/events/event_preview.dart
+++ lib/widgets/events/event_preview.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:intl/intl.dart';
 import 'package:nomad/models/event/event.dart';
 
 class EventPreview extends StatelessWidget {
@@ -13,42 +16,86 @@ class EventPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CachedNetworkImage(
-            imageUrl: event.imageUrls[0],
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 200,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  event.name,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(event.details),
-                const SizedBox(height: 8),
-                Text(
-                  'Start Time: ${event.startTime.toString()}',
-                ),
-                Text(
-                  'End Time: ${event.endTime.toString()}',
-                ),
-                if (event.placeName != null) Text(event.placeName!),
-                if (event.address != null) Text(event.address!),
-              ],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 0.0,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 0),
+              blurRadius: 8.0,
             ),
-          ),
-        ],
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(16.0)),
+              child: CachedNetworkImage(
+                imageUrl: event.imageUrls[0],
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 1 / 5,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    event.name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  if (event.startTime != null) const SizedBox(height: 16),
+                  if (event.startTime != null)
+                    Row(
+                      children: [
+                        const Icon(
+                          Iconsax.calendar_1,
+                          size: 16,
+                          color: Colors.black,
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          DateFormat('MMMM d, yyyy, hh:mm a')
+                              .format(event.startTime!),
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  if (event.placeName != null) const SizedBox(height: 16),
+                  if (event.placeName != null)
+                    Row(
+                      children: [
+                        const Icon(
+                          FontAwesomeIcons.locationArrow,
+                          size: 16,
+                          color: Colors.blue,
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          event.placeName ?? '',
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  // if (event.address != null) Text(event.address!),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
