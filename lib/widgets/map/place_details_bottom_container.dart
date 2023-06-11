0b60a900d18f4a6df2567d3937cfe9b6214d86fb
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-11 14:45:29 -0400

    feat: Fixes event problem (#11)
    TODO fix container overflow once again

--- lib/widgets/map/place_details_bottom_container.dart
+++ lib/widgets/map/place_details_bottom_container.dart
@@ -184,111 +184,116 @@ class PlaceDetailsContainerState extends State<PlaceDetailsContainer>
               topRight: Radius.circular(16.0),
             ),
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SingleChildScrollView(
+                child: Column(
                   children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width *
-                          0.2, // 25% of the screen width
-                      height: MediaQuery.of(context).size.width *
-                          0.2, // Set the height equal to the width for a circular shape
-                      decoration: BoxDecoration(
-                        color: Colors.grey[100], // Pale grey background color
-                        shape: BoxShape.circle, // Circular shape
-                      ),
-                      child: Theme(
-                        data: Theme.of(context).copyWith(
-                          iconTheme: IconThemeData(
-                            color: Colors
-                                .grey[800], // Set the desired foreground color
+                    Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width *
+                              0.2, // 25% of the screen width
+                          height: MediaQuery.of(context).size.width *
+                              0.2, // Set the height equal to the width for a circular shape
+                          decoration: BoxDecoration(
+                            color:
+                                Colors.grey[100], // Pale grey background color
+                            shape: BoxShape.circle, // Circular shape
                           ),
-                        ),
-                        child: Icon(
-                          getIconForPlaceType(widget.types[0]),
-                          size: 32.0,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.distance.isNotEmpty
-                                ? '${widget.placeName} (${widget.distance})'
-                                : widget.placeName,
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
+                          child: Theme(
+                            data: Theme.of(context).copyWith(
+                              iconTheme: IconThemeData(
+                                color: Colors.grey[
+                                    800], // Set the desired foreground color
+                              ),
+                            ),
+                            child: Icon(
+                              getIconForPlaceType(widget.types[0]),
+                              size: 32.0,
                             ),
                           ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            widget.types[0].name
-                                .replaceAll('_', ' ')
-                                .capitalize!,
-                            style: const TextStyle(fontSize: 12.0),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.distance.isNotEmpty
+                                    ? '${widget.placeName} (${widget.distance})'
+                                    : widget.placeName,
+                                style: const TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                widget.types[0].name
+                                    .replaceAll('_', ' ')
+                                    .capitalize!,
+                                style: const TextStyle(fontSize: 12.0),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                widget.address,
+                                style: const TextStyle(fontSize: 12.0),
+                              ),
+                            ],
                           ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            widget.address,
-                            style: const TextStyle(fontSize: 12.0),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RoundedIconButton(
+                          icon: FontAwesomeIcons
+                              .plus, // Replace with the appropriate Font Awesome icon
+                          label: 'Event',
+                          onPressed: () {
+                            // Perform create event action
+                            _openEventForm(context);
+                          },
+                        ),
+                        RoundedIconButton(
+                          icon: FontAwesomeIcons
+                              .locationPin, // Replace with the appropriate Font Awesome icon
+                          label: 'Directions',
+                          onPressed: () {
+                            // Perform directions action
+                          },
+                        ),
+                        RoundedIconButton(
+                          icon: FontAwesomeIcons
+                              .share, // Replace with the appropriate Font Awesome icon
+                          label: 'Share',
+                          onPressed: () {
+                            // Perform share action
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 ),
+              ),
+              if (events.isNotEmpty) ...[
                 const SizedBox(height: 8.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    RoundedIconButton(
-                      icon: FontAwesomeIcons
-                          .plus, // Replace with the appropriate Font Awesome icon
-                      label: 'Event',
-                      onPressed: () {
-                        // Perform create event action
-                        _openEventForm(context);
-                      },
-                    ),
-                    RoundedIconButton(
-                      icon: FontAwesomeIcons
-                          .locationPin, // Replace with the appropriate Font Awesome icon
-                      label: 'Directions',
-                      onPressed: () {
-                        // Perform directions action
-                      },
-                    ),
-                    RoundedIconButton(
-                      icon: FontAwesomeIcons
-                          .share, // Replace with the appropriate Font Awesome icon
-                      label: 'Share',
-                      onPressed: () {
-                        // Perform share action
-                      },
-                    ),
-                  ],
-                ),
-                if (events.isNotEmpty) ...[
-                  const SizedBox(height: 8.0),
-                  Expanded(
-                    child: ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: events.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final event = events[index];
-                        return EventPreview(event: event);
-                      },
-                    ),
+                Expanded(
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: events.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final event = events[index];
+                      return EventPreview(event: event);
+                    },
                   ),
-                ],
+                ),
               ],
-            ),
+            ],
           ),
         ),
       ),
