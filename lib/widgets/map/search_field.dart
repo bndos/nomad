5f71d506a94cb362420324503f668465541a911d
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-13 19:51:09 -0400

    refactor: Modified search components for reusability (#11)

--- lib/widgets/map/search_field.dart
+++ lib/widgets/map/search_field.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_google_places_sdk/flutter_google_places_sdk.dart'
     as places_sdk;
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nomad/services/places_service.dart';
 
-import 'package:nomad/widgets/map/autocomplete_container.dart';
+import 'package:nomad/widgets/search/search_field_ui.dart';
 
 class SearchField extends StatefulWidget {
   final GoogleMapController? mapController;
@@ -124,70 +122,12 @@ class SearchFieldState extends State<SearchField> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            width: Get.width,
-            margin: const EdgeInsets.only(top: 20, bottom: 0),
-            padding: const EdgeInsets.only(left: 5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 6,
-                  offset: Offset(0, 2),
-                ),
-              ],
-              borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(20),
-                topRight: const Radius.circular(20),
-                bottomLeft: _predictions.isEmpty
-                    ? const Radius.circular(20)
-                    : const Radius.circular(0),
-                bottomRight: _predictions.isEmpty
-                    ? const Radius.circular(20)
-                    : const Radius.circular(0),
-              ),
-            ),
-            child: Row(
-              children: [
-                if (_predictions.isNotEmpty)
-                  IconButton(
-                    icon: const Icon(Icons.clear),
-                    onPressed: _clearPredictions,
-                  )
-                else
-                  const IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: null,
-                  ),
-                Expanded(
-                  child: TextFormField(
-                    controller: _searchController,
-                    onChanged: (value) => _onSearchChanged(),
-                    style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Search',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          if (_predictions.isNotEmpty)
-            AutocompleteContainer(
-              predictionStrings: _predictionStrings,
-              handlePredictionSelection: _handlePredictionSelection,
-            ),
-        ],
-      ),
+    return SearchFieldUI(
+      searchController: _searchController,
+      onSearchChanged: _onSearchChanged,
+      predictionStrings: _predictionStrings,
+      handlePredictionSelection: _handlePredictionSelection,
+      clearPredictions: _clearPredictions,
     );
   }
 }
