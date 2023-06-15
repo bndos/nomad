f726540b9cf9a217dd6fd85e21600ae3ee22cca1
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-15 12:39:37 -0400

    hotfix: Fixes home page key error (#16)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,7 +9,7 @@ import 'package:nomad/screens/chats_screen.dart';
 import 'package:nomad/screens/profile_screen.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, Key? key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
