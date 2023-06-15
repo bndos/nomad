0d808fe03df9a1c425d6bb1f1155d00dd256793b
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-15 01:20:13 -0400

    feat: Adds a camera (#16)

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,16 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-class CameraScreen extends StatelessWidget {
-  const CameraScreen({super.key});
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  CameraScreenState createState() => CameraScreenState();
+}
+
+class CameraScreenState extends State<CameraScreen> {
+  CameraController? _cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    final backCamera = cameras.firstWhere(
+      (camera) => camera.lensDirection == CameraLensDirection.back,
+    );
+
+    _cameraController = CameraController(backCamera, ResolutionPreset.medium);
+
+    try {
+      await _cameraController!.initialize();
+    } catch (error) {
+      // Handle camera initialization error
+      print('Camera initialization failed: $error');
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _capturePhoto() async {
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
+      return;
+    }
+
+    final image = await _cameraController!.takePicture();
+
+    // Do something with the captured photo (e.g., save it, display it)
+    print('Captured photo: ${image.path}');
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Camera Screen'),
-      ),
-      body: const Center(
-        child: Text('This is the Camera Screen'),
+      body: Stack(
+        children: [
+          if (_cameraController != null &&
+              _cameraController!.value.isInitialized)
+            CameraPreview(_cameraController!),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              color: Colors.black.withOpacity(0.5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.flash_off, color: Colors.white),
+                    onPressed: () {
+                      // Toggle flash mode
+                      // Implement your flash control logic here
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.camera_alt,
+                      color: Colors.white,
+                      size: 36.0,
+                    ),
+                    onPressed: _capturePhoto,
+                  ),
+                  IconButton(
+                    icon:
+                        const Icon(Icons.flip_camera_ios, color: Colors.white),
+                    onPressed: () {
+                      // Switch camera
+                      // Implement your camera switch logic here
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
