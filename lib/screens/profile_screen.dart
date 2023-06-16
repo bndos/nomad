7299a0da5dc0fd48e660cc8609b417cedb63643b
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-16 02:34:29 -0400

    feat: Started profile page (#19)

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:iconsax/iconsax.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -9,23 +8,39 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
+        //settings left
         Align(
           alignment: Alignment.centerLeft,
           child: Container(
             margin: const EdgeInsets.only(top: 30, left: 10),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Iconsax.setting,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            margin: const EdgeInsets.only(top: 30),
             alignment: Alignment.center,
             color: Colors.transparent,
             // we can set width here with conditions
-            width: 40,
             height: kToolbarHeight,
-            child: IconButton(
-              icon: const Icon(
-                FontAwesomeIcons.chevronLeft,
-                color: Colors.black,
+            child: TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Username',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
             ),
           ),
         ),
@@ -88,99 +103,113 @@ class _ProfileScreenState extends State<ProfileScreen>
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: const MyAppBar(),
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Container(
-                  height: 200,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/profile_bg.png'),
-                      fit: BoxFit.cover,
+      body: Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.white,
+                      child: SafeArea(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin:
+                                  const EdgeInsets.only(left: 20, right: 10),
+                              height: 70,
+                              width: 70,
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(100),
+                                ),
+                              ),
+                            ),
+                            // text username
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _SliverAppBarDelegate(
+                  minHeight: 48.0,
+                  maxHeight: 48.0,
+                  child: Container(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 42.0),
+                      child: TabBar(
+                        indicatorSize: TabBarIndicatorSize.label,
+                        indicatorColor: Colors.black,
+                        controller: _tabController,
+                        tabs: const [
+                          Tab(
+                            height: 24,
+                            iconMargin: EdgeInsets.all(0),
+                            icon: Icon(
+                              Iconsax.link,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                          ),
+                          Tab(
+                            height: 24,
+                            icon: Icon(
+                              Iconsax.grid_1,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                          ),
+                          Tab(
+                            height: 24,
+                            icon: Icon(
+                              Iconsax.video_circle,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                Container(
-                  height: 100,
-                  color: Colors.white,
+              ),
+            ],
+            body: TabBarView(
+              controller: _tabController,
+              children: const [
+                // Sub events or linked events tab
+                SingleChildScrollView(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text('Sub Events or Linked Events'),
+                      ],
+                    ),
+                  ),
+                ),
+                // Pictures tab
+                SingleChildScrollView(
+                  child: Center(
+                    child: Text('Feed'),
+                  ),
+                ),
+                // Videos tab
+                SingleChildScrollView(
+                  child: Center(
+                    child: Text('Videos'),
+                  ),
                 ),
               ],
             ),
           ),
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: _SliverAppBarDelegate(
-              minHeight: 48.0,
-              maxHeight: 48.0,
-              child: Container(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 42.0),
-                  child: TabBar(
-                    indicatorSize: TabBarIndicatorSize.label,
-                    indicatorColor: Colors.black,
-                    controller: _tabController,
-                    tabs: const [
-                      Tab(
-                        height: 24,
-                        iconMargin: EdgeInsets.all(0),
-                        icon: Icon(
-                          Iconsax.link,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ),
-                      Tab(
-                        height: 24,
-                        icon: Icon(
-                          Iconsax.grid_1,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ),
-                      Tab(
-                        height: 24,
-                        icon: Icon(
-                          Iconsax.video_circle,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-        body: TabBarView(
-          controller: _tabController,
-          children: const [
-            // Sub events or linked events tab
-            SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    Text('Sub Events or Linked Events'),
-                  ],
-                ),
-              ),
-            ),
-            // Pictures tab
-            SingleChildScrollView(
-              child: Center(
-                child: Text('Feed'),
-              ),
-            ),
-            // Videos tab
-            SingleChildScrollView(
-              child: Center(
-                child: Text('Videos'),
-              ),
-            ),
-          ],
         ),
       ),
     );
