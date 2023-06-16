3964eecd5e62b07ab8dee5c7ada3bc064aa1b8e8
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-16 17:24:14 -0400

    feat: Improves scroll behaviour explore page (#19)

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -92,20 +92,103 @@ class ExploreScreenState extends State<ExploreScreen>
     return Scaffold(
       body: Stack(
         children: [
-          TabBarView(
-            controller: _tabController,
-            children: [
-              if (events.isNotEmpty) ...[
-                OverflowBox(
-                  alignment: Alignment.topCenter,
-                  maxHeight: double.infinity,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 90,
+          NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return [
+                SliverPersistentHeader(
+                  pinned: true,
+                  delegate: _SliverAppBarDelegate(
+                    minHeight: MediaQuery.of(context).padding.top + 90,
+                    maxHeight: MediaQuery.of(context).padding.top + 90,
+                    child: Container(
+                      //height is the safeara height + 90
+                      height: MediaQuery.of(context).padding.top + 90,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12.withOpacity(0.075),
+                            blurRadius: 40,
+                            offset: const Offset(0, 10),
+                          ),
+                        ],
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
                         ),
-                        child: ConstrainedBox(
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: SafeArea(
+                              child: SearchFieldUI(
+                                searchController: _searchController,
+                                onSearchChanged: _onSearchChanged,
+                                predictionStrings: _predictionStrings,
+                                handlePredictionSelection:
+                                    _handlePredictionSelection,
+                                clearPredictions: _clearPredictions,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 42.0,
+                              vertical: 4,
+                            ),
+                            child: TabBar(
+                              indicatorSize: TabBarIndicatorSize.label,
+                              controller: _tabController,
+                              indicatorColor: Colors.black,
+                              tabs: const [
+                                Tab(
+                                  height: 24,
+                                  icon: Icon(
+                                    Iconsax.link,
+                                    color: Colors.black,
+                                    size: 20,
+                                  ),
+                                ),
+                                Tab(
+                                  height: 24,
+                                  icon: Icon(
+                                    Iconsax.grid_1,
+                                    color: Colors.black,
+                                    size: 20,
+                                  ),
+                                ),
+                                Tab(
+                                  height: 24,
+                                  icon: Icon(
+                                    Iconsax.location,
+                                    color: Colors.black,
+                                    size: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ];
+            },
+            body: TabBarView(
+              controller: _tabController,
+              children: [
+                if (events.isNotEmpty) ...[
+                  OverflowBox(
+                    alignment: Alignment.topCenter,
+                    maxHeight: double.infinity,
+                    child: Column(
+                      children: [
+                        ConstrainedBox(
                           constraints: BoxConstraints(
                             maxHeight: MediaQuery.of(context).size.height - 100,
                             minHeight: 0,
@@ -119,98 +202,24 @@ class ExploreScreenState extends State<ExploreScreen>
                             },
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-              if (events.isEmpty)
-                const Center(
-                  child: Text(
-                    'No events found',
-                  ),
-                ),
-              const Center(
-                child: Text(
-                  'No posts found',
-                ),
-              ),
-              const Center(
-                child: Text(
-                  'No places found',
-                ),
-              ),
-            ],
-          ),
-          Container(
-            //height is the safeara height + 90
-            height: MediaQuery.of(context).padding.top + 90,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12.withOpacity(0.075),
-                  blurRadius: 40,
-                  offset: const Offset(0, 10),
-                ),
-              ],
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                  ),
-                  child: SafeArea(
-                    child: SearchFieldUI(
-                      searchController: _searchController,
-                      onSearchChanged: _onSearchChanged,
-                      predictionStrings: _predictionStrings,
-                      handlePredictionSelection: _handlePredictionSelection,
-                      clearPredictions: _clearPredictions,
+                      ],
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 42.0,
-                    vertical: 4,
+                ],
+                if (events.isEmpty)
+                  const Center(
+                    child: Text(
+                      'No events found',
+                    ),
                   ),
-                  child: TabBar(
-                    indicatorSize: TabBarIndicatorSize.label,
-                    controller: _tabController,
-                    indicatorColor: Colors.black,
-                    tabs: const [
-                      Tab(
-                        height: 24,
-                        iconMargin: EdgeInsets.all(0),
-                        icon: Icon(
-                          Iconsax.link,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ),
-                      Tab(
-                        height: 24,
-                        icon: Icon(
-                          Iconsax.grid_1,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ),
-                      Tab(
-                        height: 24,
-                        icon: Icon(
-                          Iconsax.location,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ),
-                    ],
+                const Center(
+                  child: Text(
+                    'No posts found',
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    'No places found',
                   ),
                 ),
               ],
@@ -224,5 +233,39 @@ class ExploreScreenState extends State<ExploreScreen>
         onPressed: () => _openEventForm(context),
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
