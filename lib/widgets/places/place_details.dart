514034df5d564ce3bb0c6a55f1bb5617ed06ee61
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-17 01:41:13 -0400

    refactor: Aliviates places details container (#19)

---
+++ lib/widgets/places/place_details.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_google_places_sdk/flutter_google_places_sdk.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:nomad/utils/place_icons.dart';
+import 'package:nomad/widgets/map/rounded_icon_button.dart';
+
+class PlaceDetails extends StatelessWidget {
+  final String placeName;
+  final String address;
+  final String distance;
+  final List<PlaceType> types;
+  final VoidCallback onAddEvent;
+  final VoidCallback onDirections;
+  final VoidCallback onShare;
+
+  const PlaceDetails({
+    Key? key,
+    required this.placeName,
+    required this.address,
+    required this.distance,
+    required this.types,
+    required this.onAddEvent,
+    required this.onDirections,
+    required this.onShare,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width *
+                  0.2, // 25% of the screen width
+              height: MediaQuery.of(context).size.width *
+                  0.2, // Set the height equal to the width for a circular shape
+              decoration: BoxDecoration(
+                color: Colors.grey[100], // Pale grey background color
+                shape: BoxShape.circle, // Circular shape
+              ),
+              child: Theme(
+                data: Theme.of(context).copyWith(
+                  iconTheme: IconThemeData(
+                    color: Colors.grey[800], // Set the desired foreground color
+                  ),
+                ),
+                child: Icon(
+                  getIconForPlaceType(types[0]),
+                  size: 32.0,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    distance.isNotEmpty ? '$placeName ($distance)' : placeName,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    types[0].name.replaceAll('_', ' ').capitalize!,
+                    style: const TextStyle(fontSize: 12.0),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    address,
+                    style: const TextStyle(fontSize: 12.0),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            RoundedIconButton(
+              icon: FontAwesomeIcons.plus,
+              label: 'Event',
+              onPressed: onAddEvent,
+            ),
+            RoundedIconButton(
+              icon: FontAwesomeIcons.locationPin,
+              label: 'Directions',
+              onPressed: onDirections,
+            ),
+            RoundedIconButton(
+              icon: FontAwesomeIcons.share,
+              label: 'Share',
+              onPressed: onShare,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
