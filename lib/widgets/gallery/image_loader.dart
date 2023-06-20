01258f2a0a7e6cb082dd760a0bc927eab1c110a6
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-19 20:12:31 -0400

    feat: Handles assets in event preview (#25)

--- lib/widgets/gallery/image_loader.dart
+++ lib/widgets/gallery/image_loader.dart
@@ -2,25 +2,57 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class ImageLoader extends StatelessWidget {
-  final String imageUrl;
+  final String? imageUrl;
+  final AssetEntity? assentEntity;
   final double width;
   final double height;
 
   const ImageLoader({
     Key? key,
-    required this.imageUrl,
     required this.width,
     required this.height,
+    this.assentEntity,
+    this.imageUrl,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (imageUrl.startsWith('http')) {
+    if (imageUrl == null && assentEntity == null) {
+      return Container(
+        width: width,
+        height: height,
+        color: Colors.grey[300],
+      );
+    }
+
+    if (assentEntity != null) {
+      return FutureBuilder<File?>(
+        future: assentEntity!.file,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Image.file(
+              snapshot.data!,
+              width: width,
+              height: height,
+              fit: BoxFit.cover,
+            );
+          }
+          return Container(
+            width: width,
+            height: height,
+            color: Colors.grey[300],
+          );
+        },
+      );
+    }
+
+    if (imageUrl!.startsWith('http')) {
       return ClipRRect(
         child: CachedNetworkImage(
-          imageUrl: imageUrl,
+          imageUrl: imageUrl!,
           fit: BoxFit.cover,
           width: width,
           height: height,
@@ -31,7 +63,7 @@ class ImageLoader extends StatelessWidget {
     } else {
       return ClipRRect(
         child: Image.file(
-          File(imageUrl),
+          File(imageUrl!),
           fit: BoxFit.cover,
           width: width,
           height: height,
