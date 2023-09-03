f68a7ef0d47c316ee398ae147e53c5e81de99ce4
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-09-02 21:53:55 -0400

    feat: Adds ability to reference event in chat (#39)

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:nomad/widgets/events/event_form.dart';
+import 'package:nomad/widgets/events/event_preview.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:nomad/models/event/event.dart' as nomad_event;
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -71,7 +74,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
                               builder: (context) {
                                 return StreamChannel(
                                   channel: channel,
-                                  child: const ChannelPage(),
+                                  child: ChannelPage(channel: channel),
                                 );
                               },
                             ),
@@ -87,10 +90,54 @@ class _ChatsScreenState extends State<ChatsScreen> {
   }
 }
 
-class ChannelPage extends StatelessWidget {
+class ChannelPage extends StatefulWidget {
+  final Channel channel;
   const ChannelPage({
     Key? key,
+    required this.channel,
   }) : super(key: key);
+
+  @override
+  State<ChannelPage> createState() => _ChannelPageState();
+}
+
+class _ChannelPageState extends State<ChannelPage> {
+  final Map<String, nomad_event.Event> _eventMap = {};
+
+  void _handleEventCreated(nomad_event.Event event) {
+    // event id generated in firestore
+
+    // Generate a numerical ID based on the size of the map
+    int eventId = _eventMap.length;
+
+    // Use the generated ID as the key in the map
+    _eventMap[eventId.toString()] = event;
+
+    widget.channel.sendMessage(
+      Message(
+        text: 'Event::${eventId.toString()};',
+      ),
+    );
+
+    // Update the UI
+    setState(() {});
+  }
+
+  void _openEventForm(
+    BuildContext context,
+  ) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (
+        BuildContext context,
+      ) {
+        return EventForm(
+          onEventCreated: _handleEventCreated,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -102,33 +149,21 @@ class ChannelPage extends StatelessWidget {
             child: StreamMessageListView(
               messageBuilder:
                   (context, details, messageList, defaultMessageWidget) {
-                if (details.message.text != null &&
-                    details.message.text!.startsWith('Event:')) {
-                  // event interaction like location, time and type
-                  return Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              padding: const EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                              child: Text(
-                                details.message.text!,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
+                final pattern =
+                    RegExp(r'Event::([^\s;]+);', caseSensitive: false);
+
+                if (details.message.text != null) {
+                  final matches = pattern.allMatches(details.message.text!);
+
+                  if (matches.isNotEmpty) {
+                    for (var match in matches) {
+                      final eventId = match.group(1);
+                      if (_eventMap.containsKey(eventId)) {
+                        final event = _eventMap[eventId];
+                        return EventPreview(event: event!);
+                      }
+                    }
+                  }
                 }
                 return defaultMessageWidget;
               },
@@ -138,12 +173,13 @@ class ChannelPage extends StatelessWidget {
             actions: [
               InkWell(
                 child: Icon(
-                  Icons.location_on,
+                  Icons.bookmark_add,
                   size: 20.0,
                   color: Colors.grey[700],
                 ),
                 onTap: () {
                   // Do something here
+                  _openEventForm(context);
                 },
               ),
             ],
