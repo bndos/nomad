9edf81aa98f1ab7c703544821cf69a37c4d200bb
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-12-26 21:25:23 -0500

    feat: Adds signin template (#6)

---
+++ lib/widgets/textfield/custom_text_field.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  String? hintText;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  CustomTextField({
+    Key? key,
+    required this.label,
+    required this.controller,
+    this.hintText,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+  }) : super(key: key);
+
+  @override
+  CustomTextFieldState createState() => CustomTextFieldState();
+}
+
+class CustomTextFieldState extends State<CustomTextField> {
+  late FocusNode _focusNode;
+  bool _isTextFieldFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.hintText ??= widget.label;
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _isTextFieldFocused = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      focusNode: _focusNode,
+      keyboardType: widget.keyboardType,
+      style: const TextStyle(color: Colors.black),
+      obscureText: widget.obscureText,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        labelStyle: TextStyle(
+          color: Colors.grey[500],
+          fontSize: 16,
+          height: 5.5,
+        ),
+        hintText: _isTextFieldFocused ? null : widget.hintText,
+        filled: true,
+        fillColor: Colors.grey[100],
+        contentPadding: EdgeInsets.symmetric(
+          vertical: _isTextFieldFocused ? 30 : 22,
+          horizontal: 24,
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(28),
+        ),
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import connectivity_plus
 import desktop_drop
 import file_selector_macos
+import firebase_auth
 import firebase_core
 import geolocator_apple
 import package_info_plus
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
